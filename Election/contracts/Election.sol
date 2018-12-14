66e75c4bb93cb3fe3d3416b5c843c80aa3966c6d
StevenAraka <stevenaraka@gmail.com>
2018-12-14 13:43:58 +0300

    Connect Ethereum to client

--- Election/contracts/Election.sol
+++ Election/contracts/Election.sol
@@ -1,12 +1,27 @@
 pragma solidity ^0.4.2;
 
 contract Election {
+     // Model a Candidate (what they look like)
+     struct Candidate { //this is like creating custom Type (interface)
+         uint id;
+         string name;
+         uint voteCount;
+     }
      // Store candidate
-     // Read candidate
-    string public candidate;
+     // Fetch candidate 
+     mapping(uint => Candidate) public candidates; // an associative array we pass (key - value)
+     // Store Candidates Count
+    uint public candidatesCount;
 
-     // Constructor 
     function Election () public {
-        candidate = "Candidate1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    // privste becsuse we dont want it to be accessible to the public interface of our contract
+    // no one else should be able to add a candidate to our mapping / election except our contract
+    function addCandidate (string _name) private { 
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0); // this instantiates a new guy of struct type Candidate
     }
 }
